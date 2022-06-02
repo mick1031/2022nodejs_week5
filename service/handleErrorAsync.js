3f4f68fac08f0c237e2888838f3f757ccf58774a
mick1031 <qqq19871031@gmail.com>
2022-06-03 01:18:34 +0800

    add npm install cross-env

---
+++ service/handleErrorAsync.js
@@ -0,0 +1,11 @@
+const handleErrorAsync = (func) => {
+    return (req, res, next) => {
+        func(req, res, next).catch(
+            (error) => {
+                return next(error);
+            }
+        );
+    };
+};
+
+module.exports = handleErrorAsync;
