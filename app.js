a91bd1b8e44936cc3fb656a5fddd3cfc16c190de
mick1031 <qqq19871031@gmail.com>
2022-06-03 01:13:16 +0800

    add handleErrorAsync.js

--- app.js
+++ app.js
@@ -35,11 +35,45 @@ app.use(function(req, res, next){
     })
 })
 
+const resErrorProd = (err, res) => {
+    if(err.isOperational){
+        res.status(err.statusCode)
+            .json({
+                message: err.message
+            })
+    }else{
+        console.log('出現重大錯誤', err);
+        res.status(500)
+            .json({
+                status: 'error',
+                message: '系統錯誤，請洽系統管理員'
+            })
+    }
+}
+
+const resErrorDev = (err, res) => {
+    res.status(err.statusCode)
+            .json({
+                message: err.message,
+                error: err,
+                stack: err.stack
+            })
+}
+
 app.use(function(err, req, res, next){
-    res.status(500)
-    .json({
-        err: err.message
-    })
+
+    // dev
+    err.statusCode = err.statusCode || 500;
+    if (process.env.NODE_ENV === 'dev') {
+        return resErrorDev(err, res)
+    }
+
+    if(err.name === 'ValidationError') {
+        err.message = '資料欄位未填寫正確，請重新輸入';
+        err.isOperational = true;
+        return resErrorProd(err, res);
+    }
+    return resErrorProd(err, res);
 })
 
 process.on('unhandledRejection', (err, promise) =>{
